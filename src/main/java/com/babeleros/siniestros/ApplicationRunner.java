1782f9a046a77e194618e2dd39e8e51f3a8a693d
oscargailb <oscar.gail@babelgroup.com>
2025-03-24 12:21:49 +0100

    runner

--- src/main/java/com/babeleros/siniestros/ApplicationRunner.java
+++ src/main/java/com/babeleros/siniestros/ApplicationRunner.java
@@ -14,14 +14,14 @@ public class ApplicationRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Ingrese el tipo de bien: ");
-        Categorias categoria = Categorias.valueOf(scanner.nextLine());
+        System.out.print("Ingrese el tipo de bien: \n");
+        Categorias categoria = Categorias.valueOf(scanner.nextLine().toUpperCase());
 
-        System.out.print("Ingrese el valor de compra del bien: ");
+        System.out.print("Ingrese el valor de compra del bien: \n");
         double valorCompra = scanner.nextDouble();
 
-        System.out.print("Ingrese la fecha de compra del bien: (formato YYYY-MM-DD)");
-        String fechaCompraStr = scanner.next();
+        System.out.print("Ingrese la fecha de compra del bien: (formato YYYY-MM-DD) \n");
+        String fechaCompraStr = scanner.nextLine();
         LocalDate fechaCompra = LocalDate.parse(fechaCompraStr);
 
         Bien bien = new Bien();
@@ -30,12 +30,13 @@ public class ApplicationRunner implements CommandLineRunner {
         bien.setValorCompra(valorCompra);
         bien.setCategoria(categoria);
 
-        System.out.println("Selecciona la compañía de seguros (Mapfre/Allianz/Mutua Madrileña): ");
+        System.out.println("Selecciona la compañía de seguros (Mapfre/Allianz/Mutua Madrileña):  \n");
         String companiaSeleccionada = scanner.nextLine();
 
         //calcular valor para el bien con la compañia elegida
+        double valorFinal = ;
 
-
+        System.out.println("\nLa valoración final del bien es: " + valorFinal + " €");
 
     }
 }
